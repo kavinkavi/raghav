05142cc7812d027a9e8546e46a0948b5a47906a3
kavinkavi <kavinincse@gmail.com>
2017-07-22 17:42:47 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+ptintf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+ptintf("the number is even");
+}
+elseif(n%2!==0)
+{
+printf("the number is even");
+}
+}
