533eaf49c780fc4d56dfbe54cf114320aab78cab
kavinkavi <kavinincse@gmail.com>
2017-07-22 18:02:08 +0530

    Create vc.c

---
+++ vc.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("enter the charecter");
+scanf("%c",&a);
+switch(a)
+{
+case 'a':
+case 'e':
+case 'i':
+case 'o':
+case 'u':
+     printf("the charecter is vowel");
+break;
+printf("the charecter is consonent");
+}
+}
